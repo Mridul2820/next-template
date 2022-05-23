3d3b382745799c6f17f124439e7beae3a4c8909d
mridul2820 <immridulpanda2820@gmail.com>
2022-05-23 10:26:36 +0530

    erroe pages and build fix

--- next.config.js
+++ next.config.js
@@ -2,20 +2,24 @@ const path = require('path');
 require('dotenv').config();
 
 module.exports = {
-  env: {},
+  env: {
+  },
   publicRuntimeConfig: {},
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: [
-      'localhost',
-      'res.cloudinary.com',
-      'inneedstore.com',
-      'i0.wp.com',
-      'images.pexels.com',
-    ],
+    domains: ['localhost', 'res.cloudinary.com'],
   },
 
-  webpack: (config, { isServer }) => {
+  exportPathMap: function () {
+    return {
+      '/locales/de-DE/summer-dress-f': {
+        page: '/locales/[locale]/[slug]',
+        query: { locale: 'de-DE', slug: 'summer-dress-f' },
+      },
+    };
+  },
+
+  webpack: (config) => {
     config.resolve.alias['components'] = path.join(__dirname, 'components');
     config.resolve.alias['public'] = path.join(__dirname, 'public');
 
