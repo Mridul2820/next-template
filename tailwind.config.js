4e403e181a210b626749363d52e892a08839c033
Mridul2820 <immridulpanda2820@gmail.com>
2023-08-25 11:19:02 +0530

    Tailwind Config Cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,25 +8,12 @@ module.exports = {
   theme: {
     screens: {
       xm: "350px",
-      // => @media (min-width: 380px) { ... }
-
       xs: "480px",
-      // => @media (min-width: 480px) { ... }
-
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
     colors: {
       transparent: "transparent",
@@ -55,11 +42,6 @@ module.exports = {
       fuchsia: colors.fuchsia,
       pink: colors.pink,
       rose: colors.rose,
-      midnight: "#121063",
-      metal: "#565584",
-      tahiti: "#3ab7bf",
-      silver: "#ecebff",
-      bermuda: "#78dcca",
     },
   },
   plugins: [],
