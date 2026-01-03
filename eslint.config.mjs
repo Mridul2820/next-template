8e97e0fb5c060c0b4ea01be042f2364c8e428769
Mridul Panda <immridulpanda2820@gmail.com>
2026-01-03 18:21:55 +0530

    feat: add configuration files for Prettier, ESLint, and Node version; update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,34 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    ".next",
   ]),
+  // Custom rule overrides
+  {
+    rules: {
+      "import/no-named-as-default": 0,
+      "react/prop-types": 0,
+      "import/extensions": 0,
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      ],
+      "react/display-name": 1,
+      "no-empty": 0,
+      "no-unused-vars": 1,
+      "no-console": 1,
+      "@next/next/no-img-element": 1,
+      "@next/next/no-html-link-for-pages": 0,
+      "import/no-anonymous-default-export": 0,
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/no-var-requires": 0,
+      "@typescript-eslint/no-unused-vars": 0,
+      "prefer-const": 0,
+      "react-hooks/exhaustive-deps": 0,
+      "@typescript-eslint/triple-slash-reference": 0,
+    },
+  },
 ]);
 
 export default eslintConfig;
